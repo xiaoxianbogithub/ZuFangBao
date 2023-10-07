2bf47fd38eda8dea344fa77b8908aa3ffc3f88db
xiao <490306224@qq.com>
2023-10-07 16:27:18 +0800

    修改配置文件 设置uuid为"dev"(仅测试接口使用) 修改启动图案为"ZuFangWang" 去除启动成功的图案

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/common/CaptchaController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/common/CaptchaController.java
@@ -55,6 +55,7 @@ public class CaptchaController
 
         // 保存验证码信息
         String uuid = IdUtils.simpleUUID();
+        uuid = "dev";
         String verifyKey = CacheConstants.CAPTCHA_CODE_KEY + uuid;
 
         String capStr = null, code = null;
